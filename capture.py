80531bd9def0a5d7ed6bbc7e998b51617aff0300
administrator <Lennon@Administrator-no-MacBook-Air.local>
2018-06-25 05:35:48 +0900

    add new file

---
+++ capture.py
@@ -0,0 +1,25 @@
+# import OpenCV
+import cv2
+
+# create instanceVideoCapture
+cap = cv2.VideoCapture(0)
+
+while True:
+    # read a frame
+    ret, frame = cap.read()
+
+
+    # show image
+    cv2.imshow('Raw Frame', frame)
+
+
+
+    # wait for user input for 1ms
+    #when the esc key input, break 
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+# release and close all windows
+cap.release()
+cv2.destroyAllWindows()
