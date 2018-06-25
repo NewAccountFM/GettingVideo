ba612940ca5fb01887bbd7f0de5561902c6dbb56
administrator <Lennon@186-169.cup.hiroshima-u.ac.jp>
2018-06-25 09:25:59 +0900

    add new file

--- capture.py
+++ capture.py
@@ -1,25 +1,27 @@
 # import OpenCV
 import cv2
+def capture_camera(mirror=True, size=None):
 
 # create instanceVideoCapture
-cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
+    
+    while True:
+     # read a frame
+        ret, frame = cap.read()
 
-while True:
-    # read a frame
-    ret, frame = cap.read()
-
-
+        if mirror is True:
+            frame = frame[:,::-1]
     # show image
-    cv2.imshow('Raw Frame', frame)
+        cv2.imshow('Raw Frame', frame)
 
 
 
     # wait for user input for 1ms
     #when the esc key input, break 
-    k = cv2.waitKey(1)
-    if k == 27:
-        break
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
 
 # release and close all windows
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
